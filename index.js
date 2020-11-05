10c55a533e4cfbc9eb151f1bbe7874860a8e253a
woodylucas <wlucas0311@gmail.com>
2020-11-05 18:06:53 -0500

    warm-ups complete

--- index.js
+++ index.js
@@ -46,3 +46,36 @@ function meeting(s) {
 		.sort()
 		.reduce((output, name) => (output += `(${name[0]}, ${name[1]})`), '');
 }
+
+/* 
+
+
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+
+*/
+
+function isValidWalk(walk) {
+	// x-axis --> increment east, decrement west
+	let x = 0;
+	// y-axis --> every time we hit north, we decrement when we do down south
+	let y = 0;
+
+	for (let direction of walk) {
+		switch (direction) {
+			case 'n':
+				y++;
+				break;
+			case 's':
+				y--;
+				break;
+			case 'e':
+				x++;
+				break;
+			case 'w':
+				x--;
+				break;
+		}
+	}
+	return walk.length === 10 && x === 0 && y === 0;
+}
