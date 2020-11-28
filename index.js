08b066ca66a637fbf989d4fced416712adab4764
woodylucas <wlucas0311@gmail.com>
2020-11-27 20:14:44 -0500

    solved 3sum

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ You are given an array(list) strarr of strings and an integer k. Your task is to
 */
 
 function longestConsec(strarr, k) {
-	let n = strarr.length;
+  let n = strarr.length;
 
-	// Base case.
-	if (n === 0 || k > n || k <= 0) return '';
-	// initialize a variable longest to an empty string to use as a comparison/storage.
-	let longest = '';
-	// iterate through the array elements
-	for (let i = 0; i < n; i++) {
-		// build a string of kth concescutive strings in array
-		let current = strarr.slice(i, k + i).join('');
-		// if the length of the current string is longer than, longest
-		if (current.length > longest.length) {
-			// store the current in longest
-			longest = current;
-		}
-	}
-	// return longest.
-	return longest;
+  // Base case.
+  if (n === 0 || k > n || k <= 0) return "";
+  // initialize a variable longest to an empty string to use as a comparison/storage.
+  let longest = "";
+  // iterate through the array elements
+  for (let i = 0; i < n; i++) {
+    // build a string of kth concescutive strings in array
+    let current = strarr.slice(i, k + i).join("");
+    // if the length of the current string is longer than, longest
+    if (current.length > longest.length) {
+      // store the current in longest
+      longest = current;
+    }
+  }
+  // return longest.
+  return longest;
 }
 
 /* 
@@ -38,13 +38,13 @@ So the result of function meeting(s) will be:
 */
 
 function meeting(s) {
-	s = s.toUpperCase();
-	// remove all the semi colons, and remove the the colons.
-	return s
-		.split(';')
-		.map((names) => names.split(':').reverse())
-		.sort()
-		.reduce((output, name) => (output += `(${name[0]}, ${name[1]})`), '');
+  s = s.toUpperCase();
+  // remove all the semi colons, and remove the the colons.
+  return s
+    .split(";")
+    .map((names) => names.split(":").reverse())
+    .sort()
+    .reduce((output, name) => (output += `(${name[0]}, ${name[1]})`), "");
 }
 
 /* 
@@ -56,28 +56,28 @@ You live in the city of Cartesia where all roads are laid out in a perfect grid.
 */
 
 function isValidWalk(walk) {
-	// x-axis --> increment east, decrement west
-	let x = 0;
-	// y-axis --> every time we hit north, we decrement when we do down south
-	let y = 0;
+  // x-axis --> increment east, decrement west
+  let x = 0;
+  // y-axis --> every time we hit north, we decrement when we do down south
+  let y = 0;
 
-	for (let direction of walk) {
-		switch (direction) {
-			case 'n':
-				y++;
-				break;
-			case 's':
-				y--;
-				break;
-			case 'e':
-				x++;
-				break;
-			case 'w':
-				x--;
-				break;
-		}
-	}
-	return walk.length === 10 && x === 0 && y === 0;
+  for (let direction of walk) {
+    switch (direction) {
+      case "n":
+        y++;
+        break;
+      case "s":
+        y--;
+        break;
+      case "e":
+        x++;
+        break;
+      case "w":
+        x--;
+        break;
+    }
+  }
+  return walk.length === 10 && x === 0 && y === 0;
 }
 
 /* 
@@ -93,14 +93,14 @@ If the input string is empty, return an empty string. The words in the input Str
 */
 
 function order(words) {
-	// ...
-	// create an array of words, and sort by digits if they match
-	return words
-		.split(' ')
-		.sort((a, b) => {
-			return a.match(/\d/) - b.match(/\d/);
-		})
-		.join(' ');
+  // ...
+  // create an array of words, and sort by digits if they match
+  return words
+    .split(" ")
+    .sort((a, b) => {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(" ");
 }
 
 /* 
@@ -112,14 +112,16 @@ Bob is preparing to pass IQ test. The most frequent task in this test is to find
 */
 
 function iqTest(numbers) {
-	// create an array of numbers using split
-	const numb = numbers.split(' ');
-	// create two arrays for one for odd, one for even
-	const odd = numb.filter((num) => num % 2 !== 0);
-	const even = numb.filter((num) => num % 2 === 0);
-	// if the length. of odd is less than the length of even
-	// find the index of that element in numb array plus 1
-	return odd.length < even.length ? numb.indexOf(odd[0]) + 1 : numb.indexOf(even[0]) + 1;
+  // create an array of numbers using split
+  const numb = numbers.split(" ");
+  // create two arrays for one for odd, one for even
+  const odd = numb.filter((num) => num % 2 !== 0);
+  const even = numb.filter((num) => num % 2 === 0);
+  // if the length. of odd is less than the length of even
+  // find the index of that element in numb array plus 1
+  return odd.length < even.length
+    ? numb.indexOf(odd[0]) + 1
+    : numb.indexOf(even[0]) + 1;
 }
 
 /* 
@@ -127,28 +129,28 @@ Complete the method/function so that it converts dash/underscore delimited words
 */
 
 function toCamelCase(str) {
-	if (!str.length) return '';
+  if (!str.length) return "";
 
-	let stringArr;
+  let stringArr;
 
-	if (str.indexOf('-') !== -1) {
-		stringArr = str.split('-');
-	} else if (str.indexOf('_') !== -1) {
-		stringArr = str.split('_');
-	}
-	// declare a varaible that will store the first element of stringArray
-	// buid an arbitrary
-	let camelCase = Array.from({length: stringArr.length}, () => '');
-	camelCase[0] = stringArr[0];
+  if (str.indexOf("-") !== -1) {
+    stringArr = str.split("-");
+  } else if (str.indexOf("_") !== -1) {
+    stringArr = str.split("_");
+  }
+  // declare a varaible that will store the first element of stringArray
+  // buid an arbitrary
+  let camelCase = Array.from({ length: stringArr.length }, () => "");
+  camelCase[0] = stringArr[0];
 
-	// iterate over camelCase, and fill the placeholders with the values from stringArray
-	for (let i = 1; i < camelCase.length; i++) {
-		const word = stringArr[i];
-		camelCase[i] += word[0].toUpperCase() + word.slice(1);
-	}
-	return camelCase.join('');
+  // iterate over camelCase, and fill the placeholders with the values from stringArray
+  for (let i = 1; i < camelCase.length; i++) {
+    const word = stringArr[i];
+    camelCase[i] += word[0].toUpperCase() + word.slice(1);
+  }
+  return camelCase.join("");
 
-	/*  REGEX SOLUTION
+  /*  REGEX SOLUTION
         const regex = /[-_]\w/gi; 
         return str.replace(regex, function(match) {
         return match.charAt(1).toUpperCase(); 
@@ -160,22 +162,22 @@ function toCamelCase(str) {
 // REGEX Practice
 
 function disemvowel(str) {
-	// g = global , i = case sensitive
-	return str.replace(/[aeiou]/gi, '');
+  // g = global , i = case sensitive
+  return str.replace(/[aeiou]/gi, "");
 }
 
 function isValidSubsequence(array, sequence) {
-	// initialize two pointers to zero
-	let arrIdx = 0;
-	let seqIdx = 0;
-	// iterate through both arrays
-	while (arrIdx < array.length && seqIdx < sequence.length) {
-		// IF the elements in both arrays are the same, increment sqxIdx
-		if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
-		arrIdx++;
-	}
-	// if sqxIdx is the same value as sequence array length
-	return seqIdx === sequence.length;
+  // initialize two pointers to zero
+  let arrIdx = 0;
+  let seqIdx = 0;
+  // iterate through both arrays
+  while (arrIdx < array.length && seqIdx < sequence.length) {
+    // IF the elements in both arrays are the same, increment sqxIdx
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  // if sqxIdx is the same value as sequence array length
+  return seqIdx === sequence.length;
 }
 
 /*
@@ -204,45 +206,45 @@ If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you wan
 
 */
 
-var isSubsequence = function(s, t) {
-	let sIdx = 0;
-	let tIdx = 0;
-	while (sIdx < s.length && tIdx < t.length) {
-		if (s[sIdx] === t[tIdx]) sIdx++;
-		tIdx++;
-	}
-	return sIdx === s.length;
+var isSubsequence = function (s, t) {
+  let sIdx = 0;
+  let tIdx = 0;
+  while (sIdx < s.length && tIdx < t.length) {
+    if (s[sIdx] === t[tIdx]) sIdx++;
+    tIdx++;
+  }
+  return sIdx === s.length;
 };
 
 // O(n^2) time | O(n) space
 function threeNumberSum(array, targetSum) {
-	// sort the array
-	array.sort((a, b) => a - b);
-	// declare a const variable triplets that is initialize to an empty array.
-	const triplets = [];
-	// iterate through array, break when the array has to be the third value from the end of the array.
-	for (let i = 0; i < array.length - 2; i++) {
-		if (array[i] === array[i - 1]) continue;
-		// initialize a left pointer, and a right pointer.
-		let left = i + 1;
-		let right = array.length - 1;
-		// While both our pointers aren't overlapping each other...
-		while (left < right) {
-			const currentSum = array[i] + array[left] + array[right];
-			if (currentSum === targetSum) {
-				triplets.push([ array[i], array[left], array[right] ]);
-				left++;
-				right--;
-				while (left < right && array[left] === array[left - 1]) left++;
-			} else if (currentSum < targetSum) {
-				left++;
-			} else if (currentSum > targetSum) {
-				right--;
-			}
-		}
-	}
-	// RETURN triplets.
-	return triplets;
+  // sort the array
+  array.sort((a, b) => a - b);
+  // declare a const variable triplets that is initialize to an empty array.
+  const triplets = [];
+  // iterate through array, break when the array has to be the third value from the end of the array.
+  for (let i = 0; i < array.length - 2; i++) {
+    if (array[i] === array[i - 1]) continue;
+    // initialize a left pointer, and a right pointer.
+    let left = i + 1;
+    let right = array.length - 1;
+    // While both our pointers aren't overlapping each other...
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+        while (left < right && array[left] === array[left - 1]) left++;
+      } else if (currentSum < targetSum) {
+        left++;
+      } else if (currentSum > targetSum) {
+        right--;
+      }
+    }
+  }
+  // RETURN triplets.
+  return triplets;
 }
 
 /* 
@@ -256,12 +258,12 @@ Zero isn't an odd number and you don't need to move it. If you have an empty arr
 */
 
 function sortArray(array) {
-	// create an odd array
-	// sort the array
-	const odd = array.filter((elem) => elem % 2 !== 0).sort((a, b) => a - b);
-	// IF the element is odd remove from the odd array and add to shifted array,
-	// otherwise even keep it at it's position.
-	return array.map((elem) => (elem % 2 !== 0 ? odd.shift() : elem)); // Return a sorted odd only array.
+  // create an odd array
+  // sort the array
+  const odd = array.filter((elem) => elem % 2 !== 0).sort((a, b) => a - b);
+  // IF the element is odd remove from the odd array and add to shifted array,
+  // otherwise even keep it at it's position.
+  return array.map((elem) => (elem % 2 !== 0 ? odd.shift() : elem)); // Return a sorted odd only array.
 }
 
 /* 
@@ -280,22 +282,57 @@ Same meaning: 1 == 1
 */
 
 function sumConsecutives(s) {
-	// declare variable sumConsec initialize to an empty array to store the sums of the consecutives
-	const sumConsec = [];
-	// declare a temp variable initialize to 0
-	let temp = 0;
-	// iterate through the s array an obtain all the elements
-	for (let i = 0; i < s.length; i++) {
-		// declare two variables current, next
-		const current = s[i];
-		const next = s[i + 1];
-		// IF current is the same element as next
-		if (current === next) {
-			temp += current; // accumulate the sum in temp, and push into sumConsec array.
-		} else {
-			sumConsec.push(temp + current); // Push temp + current into sumConsec
-			temp = 0; // Set temp to zero to establish a new accumulator
-		}
-	}
-	return sumConsec;
+  // declare variable sumConsec initialize to an empty array to store the sums of the consecutives
+  const sumConsec = [];
+  // declare a temp variable initialize to 0
+  let temp = 0;
+  // iterate through the s array an obtain all the elements
+  for (let i = 0; i < s.length; i++) {
+    // declare two variables current, next
+    const current = s[i];
+    const next = s[i + 1];
+    // IF current is the same element as next
+    if (current === next) {
+      temp += current; // accumulate the sum in temp, and push into sumConsec array.
+    } else {
+      sumConsec.push(temp + current); // Push temp + current into sumConsec
+      temp = 0; // Set temp to zero to establish a new accumulator
+    }
+  }
+  return sumConsec;
 }
+
+var threeSum = function (nums) {
+  // a + b + c = 0
+  // find all UNIQUE triplets in array. No elements can repeat itself.
+  // We need to establish 3 pointers.
+  const triplets = []; // declare a varible triplets set to an empty array.
+  nums.sort((a, b) => a - b); // sort nums array to establish an easier search
+  // iterate over nums array:
+  for (let i = 0; i < nums.length - 2; i++) {
+    const num = nums[i];
+    if (num > 0) return triplets; // if the number in the nums array is greater than zero return an empty array
+    if (nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const currentSum = nums[i] + nums[left] + nums[right];
+      if (currentSum === 0) {
+        triplets.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (left < right && nums[left] === nums[left - 1]) left++;
+        while (left < right && nums[right] === nums[right + 1]) right--;
+      } else if (currentSum > 0) {
+        right--;
+      } else if (currentSum < 0) {
+        left++;
+      }
+    }
+  }
+
+  // if the current element is the same as the previous skip, that element.
+  // establish a left pointer, and a right pointer. Initialize left to the next element, and right to the the last element in the array.
+
+  return triplets;
+};
