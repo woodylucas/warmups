0808636798abdf6cb0814af9987d0198abfbb09d
woodylucas <wlucas0311@gmail.com>
2020-12-03 23:10:03 -0500

    refactored solution

--- index.js
+++ index.js
@@ -420,3 +420,14 @@ function breaksDirection(direction, previousInt, currentInt) {
   }
   return difference > 0;
 }
+
+function isMonotonic2(array) {
+  // Write your code here.
+  let isNonDecreasing = true;
+  let isNonIncreasing = true;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[i - 1]) isNonDecreasing = false;
+    if (array[i] > array[i - 1]) isNonDecreasing = false;
+  }
+  return isNonDecreasing || isNonIncreasing;
+}
