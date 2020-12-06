779e108bd2b4d26ce38b1b4ccb72fbbb42c40243
woodylucas <wlucas0311@gmail.com>
2020-12-06 11:17:51 -0500

    matrices problem solved

--- index.js
+++ index.js
@@ -431,3 +431,35 @@ function isMonotonic2(array) {
   }
   return isNonDecreasing || isNonIncreasing;
 }
+
+function spiralTraverse(array) {
+  const result = [];
+  let [startRow, endRow] = [0, array.length - 1];
+  let [startCol, endCol] = [0, array[0].length - 1];
+
+  while (startRow <= endRow && startCol <= endCol) {
+    console.log(array[endCol - 1]);
+    for (let col = startCol; col <= endCol; col++) {
+      result.push(array[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      result.push(array[row][endCol]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      if (startRow === endRow) break;
+      result.push(array[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row > startRow; row--) {
+      if (startCol === endCol) break;
+      result.push(array[row][startCol]);
+    }
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+  return result;
+}
