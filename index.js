0e847414ed2c6876a136130af86cf417b2f38ee1
woodylucas <wlucas0311@gmail.com>
2021-01-06 17:23:36 -0500

    trapped water solution

--- index.js
+++ index.js
@@ -941,3 +941,37 @@ function FirstReverse(str) {
 
   return reverse.join("");
 }
+
+function waterArea(heights) {
+  /*
+	0 8 0 0 0 5 0 0 10 0 1 1 0 3  
+	*/
+
+  // Edge case:
+  if (heights.length === 0) return 0;
+
+  // Declare a Arbitrary array filled with zero w/ array
+  const maxes = Array.from({ length: heights.length }, () => 0);
+
+  let leftMax = 0;
+  // Obtain the maxes that are to the left of array
+  for (let i = 0; i < heights.length; i++) {
+    const height = heights[i];
+    maxes[i] = leftMax;
+    leftMax = Math.max(leftMax, height);
+  }
+  let rightMax = 0;
+  // Obtain the maxes to the right of the array
+  for (let i = heights.length - 1; i >= 0; i--) {
+    const height = heights[i];
+    let minHeight = Math.min(rightMax, maxes[i]);
+
+    if (height < minHeight) {
+      maxes[i] = minHeight - height;
+    } else {
+      maxes[i] = 0;
+    }
+    rightMax = Math.max(rightMax, height);
+  }
+  return maxes.reduce((a, b) => a + b, 0);
+}
