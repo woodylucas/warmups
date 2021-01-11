cba110014132965f1b719a55b747a640727e1770
woodylucas <wlucas0311@gmail.com>
2021-01-10 21:23:32 -0500

    isPalindrome solved, both iterativly and recursivly

--- recursion.js
+++ recursion.js
@@ -1,8 +1,7 @@
-function reverse(string, reverseStr = ''){
-  if (string.length === 0) return reverseStr; 
-  return reverse(string.slice(1), string[0] + reverseStr); 
+function reverse(string, reverseStr = "") {
+  if (string.length === 0) return reverseStr;
+  return reverse(string.slice(1), string[0] + reverseStr);
 }
-
 
 /*
 
@@ -14,3 +13,31 @@ function reverse(string){
     return reverse; 
 
 }
+
+*/
+
+function isPalindrome(string, leftIdx = 0, rightIdx = string.length - 1) {
+  if (leftIdx < rightIdx) {
+    if (string[leftIdx] !== string[rightIdx]) return false;
+    isPalindrome(string, leftIdx + 1, rightIdx - 1);
+  }
+  return true;
+}
+
+/*
+
+iterative 
+
+function isPalindrome(string){
+  // add whatever parameters you deem necessary - good luck!
+  let [leftIdx, rightIdx] = [0, string.length -1]; 
+  
+  while (leftIdx < rightIdx) {
+      if (string[leftIdx] !== string[rightIdx]) return false; 
+      leftIdx++; 
+      rightIdx--; 
+  }
+  return true; 
+}
+
+*/
