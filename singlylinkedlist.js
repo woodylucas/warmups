3bbae0c4eb1337fef1005be72267a28bbc241d8a
woodylucas <wlucas0311@gmail.com>
2021-01-12 23:32:54 -0500

    midpoint

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -98,3 +98,17 @@ class LinkedList {
     }
   }
 }
+
+// Midpoint
+
+function midpoint(list) {
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
