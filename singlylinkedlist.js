0eff0460fac58dc6e3b26379ee7d3abaf3c8848b
woodylucas <wlucas0311@gmail.com>
2021-01-13 14:23:06 -0500

    from  last

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -132,3 +132,19 @@ function circular(list) {
   }
   return false;
 }
+
+function fromLast(list, n) {
+  let slow = list.head;
+  let fast = list.head;
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+}
