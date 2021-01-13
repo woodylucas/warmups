bc099462f7a1034c207aa06d23bba457e3b115c6
woodylucas <wlucas0311@gmail.com>
2021-01-12 19:37:35 -0500

    implemented a size for linked list

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -15,4 +15,15 @@ class LinkedList {
     // do not override existing node
     this.head = new Node(data, this.head);
   }
+  // size method which counts the nodes
+  size() {
+    let count = 0;
+    let node = this.head; // Declare a node variable initialize to this.head
+    // While the node exist
+    while (node) {
+      count++; // Increment count variable
+      node = node.next; // reinitialize node to next node (if node.next is null the loop will break)
+    }
+    return count; // return count (which is the size)
+  }
 }
