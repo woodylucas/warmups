5aac5a3013745223cf5820c58c7083a09e2bbce2
woodylucas <wlucas0311@gmail.com>
2021-01-14 23:42:16 -0500

    solved

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -180,3 +180,40 @@ function removeKthNodeFromEnd(head, k) {
 
   slow.next = slow.next.next;
 }
+
+// Add two numbers solution
+var addTwoNumbers = function (l1, l2) {
+  let head = new ListNode(0);
+  let node = head;
+  let carry = 0;
+
+  while (l1 || l2) {
+    let l1Value = l1 ? l1.val : 0;
+    let l2Value = l2 ? l2.val : 0;
+
+    let sum = l1Value + l2Value + carry;
+    carry = 0;
+    let newValue = sum;
+
+    if (sum > 9) {
+      newValue = sum % 10;
+      carry = 1;
+    }
+
+    node.next = new ListNode(newValue);
+    node = node.next;
+
+    if (l1) {
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  if (carry) {
+    node.next = new ListNode(carry);
+  }
+
+  return head.next;
+};
