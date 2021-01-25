1cbb5230459311a13c02cce1b5d8fe660d202f66
woodylucas <wlucas0311@gmail.com>
2021-01-25 18:44:14 -0500

    tree traversal

--- trees.js
+++ trees.js
@@ -3,8 +3,31 @@ class Node {
     this.data = data;
     this.children = [];
   }
-
+  // Add method:
   add(data) {
     this.children.push(new Node(data));
+  }
+
+  // Remove method:
+  remove(data) {
+    this.children = this.children.filter((node) => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
   }
 }
